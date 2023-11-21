882350f71551ca8a550edaa8d944924b58194748
Exequiel Raineri <exeraineri15@gmail.com>
2023-11-21 00:42:46 -0300

    deploy

--- src/componentes/Encabezado.jsx
+++ src/componentes/Encabezado.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
-
 export const Encabezado = () => {
   return (
     <nav className='encabezado'>
-      <img src="src/img/logo.png" alt="Logo Movie Web App" />
+      <img src="./img/logo.png" alt="Logo Movie Web App" />
       <h1 className='titulo'>Movie Web App</h1>
     </nav >
   )
